5be8450ff40bd4f552abb3422d7ab3a3c6594691
nick <s114623@ap.be>
2020-10-22 23:24:50 +0200

    Adjusted the classes

--- app/src/main/java/com/example/cosplan/ui/test/testFragment.java
+++ app/src/main/java/com/example/cosplan/ui/test/testFragment.java
@@ -2,65 +2,33 @@ package com.example.cosplan.ui.test;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.cosplan.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link testFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class testFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public testFragment() {
-        // Required empty public constructor
+    private testViewModel testViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup containder,Bundle savedInstanceState){
+        testViewModel= ViewModelProviders.of(this).get(com.example.cosplan.ui.test.testViewModel.class);
+        View root=inflater.inflate(R.layout.fragment_test,containder,false);
+        final TextView textView=root.findViewById(R.id.text_test);
+        testViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment test.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static testFragment newInstance(String param1, String param2) {
-        testFragment fragment = new testFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
-    }
 }
--- app/src/main/java/com/example/cosplan/ui/test/testViewModel.java
+++ app/src/main/java/com/example/cosplan/ui/test/testViewModel.java
@@ -1,4 +1,18 @@
 package com.example.cosplan.ui.test;
 
-public class testViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class testViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public testViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("this is the test fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
