fd29ebb8f26cd4d650548972dc17ada4dd3c908c
nick <s114623@ap.be>
2020-10-23 16:31:00 +0200

    Changed the second option to Webshops

--- app/src/main/java/com/example/cosplan/ui/webshop/WebshopFragment.java
+++ app/src/main/java/com/example/cosplan/ui/webshop/WebshopFragment.java
@@ -1,4 +1,4 @@
-package com.example.cosplan.ui.slideshow;
+package com.example.cosplan.ui.webshop;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cosplan.R;
 
-public class SlideshowFragment extends Fragment {
+public class WebshopFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private WebshopViewModel webshopViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        webshopViewModel =
+                ViewModelProviders.of(this).get(WebshopViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_webshop, container, false);
+        final TextView textView = root.findViewById(R.id.text_webshop);
+        webshopViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
