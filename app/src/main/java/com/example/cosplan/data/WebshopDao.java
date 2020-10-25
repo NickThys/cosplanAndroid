e3569b5e29b1eae0d9c3c6c429638cf9eab39e1c
nick <s114623@ap.be>
2020-10-25 09:42:27 +0100

    Added webshop Dao

---
+++ app/src/main/java/com/example/cosplan/data/WebshopDao.java
@@ -0,0 +1,16 @@
+package com.example.cosplan.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WebshopDao {
+    @Insert
+    void insert(Webshop webshop);
+    @Query("SELECT * FROM webshop_table ORDER BY name ASC")
+    LiveData<List<Webshop>> getAllWebshops();
+}
