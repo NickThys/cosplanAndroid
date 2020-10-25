b5d86214152e8868300fb1d8f1697745ab3f78cf
nick <s114623@ap.be>
2020-10-25 09:50:30 +0100

    Added webshopDatabase

---
+++ app/src/main/java/com/example/cosplan/data/WebshopDatabase.java
@@ -0,0 +1,22 @@
+package com.example.cosplan.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+@Database(entities = Webshop.class,version = 1, exportSchema = false)
+abstract class WebshopDatabase extends RoomDatabase {
+    public abstract  WebshopDao webshopDao();
+    private static WebshopDatabase INSTANCE;
+    public static WebshopDatabase getDatabase(final Context context){
+        if (INSTANCE==null){
+            synchronized (WebshopDatabase.class){
+                if (INSTANCE == null) {
+                    INSTANCE= Room.databaseBuilder(context.getApplicationContext(),WebshopDatabase.class,"webshopDatabase").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
