a7da6f306b49d41b787623be297c998baedd4f99
nick <s114623@ap.be>
2020-10-26 18:25:55 +0100

    Added convention

---
+++ app/src/main/java/com/example/cosplan/data/Convention/Convention.java
@@ -0,0 +1,30 @@
+package com.example.cosplan.data.Convention;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+
+import com.google.type.Date;
+
+
+@Entity(tableName = "convention_table")
+public class Convention {
+    @NonNull
+    @ColumnInfo(name = "Name")
+    public String mConName;
+    @NonNull
+    @ColumnInfo(name = "BeginDate")
+    public Date mConBeginDate;
+    @NonNull
+    @ColumnInfo(name = "EndDate")
+    public Date mConEndDate;
+    @NonNull
+    @ColumnInfo(name = "Place")
+    public String mConPlace;
+
+    public String getConventionName(){return this.mConName;}
+    public Date getConventionBeginDate(){return this.mConBeginDate;}
+    public Date getConventionEndDate(){return this.mConEndDate;}
+    public String getConventionPlace(){return this.mConPlace;}
+
+}
