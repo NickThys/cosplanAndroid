78b52f42b57790050f3b70c2b19c6b4cc4e91b6d
nick <s114623@ap.be>
2020-10-26 18:32:14 +0100

    Added an enum to convention

--- app/src/main/java/com/example/cosplan/data/Convention/Convention.java
+++ app/src/main/java/com/example/cosplan/data/Convention/Convention.java
@@ -6,7 +6,7 @@ import androidx.room.Entity;
 
 import com.google.type.Date;
 
-
+enum Country{Belgium,Netherlands}
 @Entity(tableName = "convention_table")
 public class Convention {
     @NonNull
@@ -19,12 +19,16 @@ public class Convention {
     @ColumnInfo(name = "EndDate")
     public Date mConEndDate;
     @NonNull
+    @ColumnInfo(name = "Country")
+    public Country mConCountry;
+    @NonNull
     @ColumnInfo(name = "Place")
     public String mConPlace;
 
     public String getConventionName(){return this.mConName;}
     public Date getConventionBeginDate(){return this.mConBeginDate;}
     public Date getConventionEndDate(){return this.mConEndDate;}
+    public Country getConventionCountry(){return this.mConCountry;}
     public String getConventionPlace(){return this.mConPlace;}
 
 }
