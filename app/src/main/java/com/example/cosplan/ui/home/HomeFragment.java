03f4f4520e4a39f6aaed46688eaae48c8aeffecc
nick <s114623@ap.be>
2020-10-28 14:25:18 +0100

    Popup dialog works
    Still need to add pickers for the start and enddate

--- app/src/main/java/com/example/cosplan/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cosplan/ui/home/HomeFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,7 +22,9 @@ public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
-
+    private EditText mCosplayName,mCosplayStartDate,mCosplayEndDate,mCosplayBudget;
+    private ImageView mCosplayImage;
+    private Button mChoosePicture,mCancel,mAddNewCosplay;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
@@ -30,6 +35,7 @@ public class HomeFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+                createNewCosplayDialog();
             }
         });
         return root;
@@ -37,5 +43,24 @@ public class HomeFragment extends Fragment {
     public void createNewCosplayDialog(){
         dialogBuilder=new AlertDialog.Builder(requireContext());
         final View cosplayPopUpView=getLayoutInflater().inflate(R.layout.add_cosplay,null);
+        mCosplayName=cosplayPopUpView.findViewById(R.id.EditTextCosplayName);
+        mCosplayStartDate=cosplayPopUpView.findViewById(R.id.editTextBeginDate);
+        mCosplayEndDate=cosplayPopUpView.findViewById(R.id.editTextEndDate);
+        mCosplayBudget=cosplayPopUpView.findViewById(R.id.editTextBudget);
+        mCosplayImage=cosplayPopUpView.findViewById(R.id.imageViewCosplayImgPreview);
+        mChoosePicture=cosplayPopUpView.findViewById(R.id.btnChooseCosplayImg);
+        mCancel=cosplayPopUpView.findViewById(R.id.buttonCancel);
+        mAddNewCosplay=cosplayPopUpView.findViewById(R.id.buttonAddCosplay);
+
+        dialogBuilder.setView(cosplayPopUpView);
+        dialog=dialogBuilder.create();
+        dialog.show();
+
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
     }
 }
