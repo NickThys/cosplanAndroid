a27ade9e7d812278e390bab195e3b6976a09f2e5
nick <s114623@ap.be>
2020-11-10 18:18:16 +0100

    Added cosplay wip.pic into the copslayscreen & added the onclick handeler

--- app/src/main/java/com/example/cosplan/ui/cosplay_screen/cosplayScreen.java
+++ app/src/main/java/com/example/cosplan/ui/cosplay_screen/cosplayScreen.java
@@ -124,6 +124,14 @@ public class cosplayScreen extends Fragment {
                 fl.addView(view);
             }
         });
+        mCosplayWIPPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fl.removeAllViews();
+                View view=getLayoutInflater().inflate(R.layout.fragment_cosplay_wip_img,null);
+                fl.addView(view);
+            }
+        });
         return v;
     }
     public void UpdateCosplayDialog(final Cosplay cosplay){
