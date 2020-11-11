1843f63b58621c2117f3b2f6928b2f7792d09ea7
nick <s114623@ap.be>
2020-11-11 10:34:22 +0100

    Viewmodel finished for real
    Didn't staged the changes

--- app/src/main/java/com/example/cosplan/data/Cosplay_Part/PartViewModel.java
+++ app/src/main/java/com/example/cosplan/data/Cosplay_Part/PartViewModel.java
@@ -1,4 +1,24 @@
 package com.example.cosplan.data.Cosplay_Part;
 
-public class PartViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class PartViewModel extends AndroidViewModel {
+private PartRepository mRepository;
+LiveData<List<Part>>mAllPartsToMake;
+LiveData<List<Part>>mAllPartsToBuy;
+    public PartViewModel(@NonNull Application application) {
+        super(application);
+        mRepository=new PartRepository(application);
+        mAllPartsToMake=mRepository.getAllPartsToMake();
+        mAllPartsToBuy=mRepository.getAllPartsToBuy();
+    }
+    public LiveData<List<Part>>getAllPartsToMake(){return mAllPartsToMake;}
+    public LiveData<List<Part>>getAllPartsToBoy(){return mAllPartsToBuy;}
+    public void insert(Part part){mRepository.insert(part);}
 }
