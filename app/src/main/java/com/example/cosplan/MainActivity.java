a610446cc5d4ca5849e77ecb7b47e0fa5dab6197
nick <s114623@ap.be>
2020-11-12 10:59:14 +0100

    Made a small change

--- app/src/main/java/com/example/cosplan/MainActivity.java
+++ app/src/main/java/com/example/cosplan/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.Toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
