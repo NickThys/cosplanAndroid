7b20d0fc9013de3c730083490bda76f9e51232e3
nick <s114623@ap.be>
2020-11-21 14:27:06 +0100

    Added WIPImg entity

---
+++ app/src/main/java/com/example/cosplan/data/Coplay/WIPImg/WIPImg.java
@@ -0,0 +1,35 @@
+package com.example.cosplan.data.Coplay.WIPImg;
+
+import android.graphics.Bitmap;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import com.example.cosplan.data.Coplay.Cosplay;
+
+@Entity(tableName = "CosplayRefImg_table",
+        foreignKeys = @ForeignKey(entity = Cosplay.class,
+                parentColumns = "Id",
+                childColumns = "CosplayId"))
+public class WIPImg {
+    @ColumnInfo(name = "CosplayId")
+    public int mCosplayId;
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "CosplayWIPImgId")
+    public int mCosplayWIPImgIg;
+
+    @ColumnInfo(name = "CosplayWIPImgImage")
+    public Bitmap mCosplayWIPImgImage;
+
+    public WIPImg(int mCosplayId, int mCosplayWIPImgIg, Bitmap mCosplayWIPImgImage) {
+        this.mCosplayId = mCosplayId;
+        this.mCosplayWIPImgIg = mCosplayWIPImgIg;
+        this.mCosplayWIPImgImage = mCosplayWIPImgImage;
+    }
+
+    public WIPImg() {
+    }
+}
