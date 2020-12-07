be7a0d2f2a64ac3ab1e2d277a6e6656c95f2b16b
nick <s114623@ap.be>
2020-12-07 17:57:29 +0100

    Refactored the ConventionDatabase

--- app/src/main/java/com/example/cosplan/data/Convention/ConventionDatabase.java
+++ app/src/main/java/com/example/cosplan/data/Convention/ConventionDatabase.java
@@ -11,20 +11,19 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Convention.class,version = 1,exportSchema = false)
 abstract class ConventionDatabase extends RoomDatabase {
-    public abstract ConventionDao conventionDao();
+    public abstract ConventionDao mConventionDao();
     private static ConventionDatabase INSTANCE;
     public static ConventionDatabase getDatabase(final Context context){
         if(INSTANCE==null){
             synchronized (ConventionDatabase.class){
                 if (INSTANCE==null){
                     INSTANCE= Room.databaseBuilder(context.getApplicationContext(),ConventionDatabase.class,"conventionDatabase").addCallback(sRoomDatabaseCallback).build();
-
                 }
             }
         }
         return INSTANCE;
     }
-    private static RoomDatabase.Callback sRoomDatabaseCallback=new RoomDatabase.Callback(){
+    private static final RoomDatabase.Callback sRoomDatabaseCallback=new RoomDatabase.Callback(){
         @Override
         public void onOpen(@NonNull SupportSQLiteDatabase db) {
             super.onOpen(db);
@@ -33,39 +32,43 @@ abstract class ConventionDatabase extends RoomDatabase {
     };
 
     private static class PopulateDb extends AsyncTask<Void,Void,Void>{
-        private final ConventionDao mdao;
-        PopulateDb(ConventionDatabase db){mdao= db.conventionDao();}
-        String []ConNamesBel={"Comic Con Brussels","Facts Spring","Made in Asia","Comic Con Antwerp","Elftopia"};
-        String []ConCountry={"Belgium","Netherland"};
-        String []ConStartDateBel={"13/02/2021","3/04/2021","12/03/2021","05/05/2021","08/08/2021"};
-        String []ConEndDateBel={"14/02/2021","4/04/2021","14/03/2021","06/05/2021","09/08/2021"};
-        String []ConPlaceBel={"Tour & Taxis","Flanders Expo","Brussels Expo","Waagnatie","kasteel Ooidonk"};
+        private final ConventionDao mConventiondao;
+        PopulateDb(ConventionDatabase mConventionDatabase){
+            mConventiondao = mConventionDatabase.mConventionDao();}
+        //region Convention data
+        private final String [] ConventionCountry ={"Belgium","Netherland"};
 
-        String []ConNamesNl={"Comic Con Ahoy","Dutch Comic Con","Magical Castle Event","Comic Con Amsterdam"};
-        String []ConStartDateNl={"27/02/2021","27/03/2021","29/50/2021","28/08/2021"};
-        String []ConEndDateNl={"28/02/2021","28/03/2021","30/05/2021","29/08/2021"};
-        String []ConPlaceNl={"Ahoy rotterdam","Jaarbeurs Utrecht","Park Spelderholt","RAI Amsterdam"};
+        private final String [] ConventionNamesBelgium ={"Comic Con Brussels","Facts Spring","Made in Asia","Comic Con Antwerp","Elftopia"};
+        private final String [] ConventionStartDateBelgium ={"13/02/2021","3/04/2021","12/03/2021","05/05/2021","08/08/2021"};
+        private final String [] ConventionEndDateBelgium ={"14/02/2021","4/04/2021","14/03/2021","06/05/2021","09/08/2021"};
+        private final String [] ConventionPlaceBelgium ={"Tour & Taxis","Flanders Expo","Brussels Expo","Waagnatie","kasteel Ooidonk"};
+
+        private final String [] ConventionNamesNetherland ={"Comic Con Ahoy","Dutch Comic Con","Magical Castle Event","Comic Con Amsterdam"};
+        private final String [] ConventionStartDateNetherland ={"27/02/2021","27/03/2021","29/50/2021","28/08/2021"};
+        private final String [] ConventionEndDateNetherland ={"28/02/2021","28/03/2021","30/05/2021","29/08/2021"};
+        private final String [] ConventionPlaceNetherland ={"Ahoy rotterdam","Jaarbeurs Utrecht","Park Spelderholt","RAI Amsterdam"};
+        //endregion
 
         @Override
         protected Void doInBackground(Void... voids) {
-            if (mdao.getAnyConvention().length<1){
-                for (int i=0;i<ConNamesBel.length;i++){
-                    Convention con=new Convention();
-                    con.mConName=ConNamesBel[i];
-                    con.mConCountry=ConCountry[0];
-                    con.mConBeginDate=ConStartDateBel[i];
-                    con.mConEndDate=ConEndDateBel[i];
-                    con.mConPlace=ConPlaceBel[i];
-                    mdao.insert(con);
+            if (mConventiondao.getAnyConvention().length<1){
+                for (int i = 0; i< ConventionNamesBelgium.length; i++){
+                    Convention mTempConventionBelgium=new Convention();
+                    mTempConventionBelgium.mConventionName = ConventionNamesBelgium[i];
+                    mTempConventionBelgium.mConventionCountry = ConventionCountry[0];
+                    mTempConventionBelgium.mConventionBeginDate = ConventionStartDateBelgium[i];
+                    mTempConventionBelgium.mConventionEndDate = ConventionEndDateBelgium[i];
+                    mTempConventionBelgium.mConventionPlace = ConventionPlaceBelgium[i];
+                    mConventiondao.insert(mTempConventionBelgium);
                 }
-                for (int i=0;i<ConNamesNl.length;i++){
-                    Convention con=new Convention();
-                    con.mConName=ConNamesNl[i];
-                    con.mConCountry=ConCountry[1];
-                    con.mConBeginDate=ConStartDateNl[i];
-                    con.mConEndDate=ConEndDateNl[i];
-                    con.mConPlace=ConPlaceNl[i];
-                    mdao.insert(con);
+                for (int i = 0; i< ConventionNamesNetherland.length; i++){
+                    Convention mTempConventionNetherland=new Convention();
+                    mTempConventionNetherland.mConventionName = ConventionNamesNetherland[i];
+                    mTempConventionNetherland.mConventionCountry = ConventionCountry[1];
+                    mTempConventionNetherland.mConventionBeginDate = ConventionStartDateNetherland[i];
+                    mTempConventionNetherland.mConventionEndDate = ConventionEndDateNetherland[i];
+                    mTempConventionNetherland.mConventionPlace = ConventionPlaceNetherland[i];
+                    mConventiondao.insert(mTempConventionNetherland);
                 }
             }
             return null;
