25563ca2b80cad835879156288dfd57f6ab6e98f
nick <s114623@ap.be>
2020-12-08 18:05:14 +0100

    Refactored the viewmodel

--- app/src/main/java/com/example/cosplan/data/cosplay/checkList/CheckListPartViewModel.java
+++ app/src/main/java/com/example/cosplan/data/cosplay/checkList/CheckListPartViewModel.java
@@ -9,22 +9,25 @@ import androidx.lifecycle.LiveData;
 import java.util.List;
 
 public class CheckListPartViewModel extends AndroidViewModel {
-    private CheckListPartRepository mRepository;
+    private final CheckListPartRepository mCheckListPartRepository;
     LiveData<List<ChecklistPart>> mAllCheckListParts;
     private int mCosplayId;
 
     public CheckListPartViewModel(@NonNull Application application) {
         super(application);
-        mRepository=new CheckListPartRepository(application);
-        mAllCheckListParts=mRepository.getAllCheckListParts(mCosplayId);
+        mCheckListPartRepository =new CheckListPartRepository(application);
+        mAllCheckListParts= mCheckListPartRepository.getAllCheckListParts(mCosplayId);
     }
 
     public LiveData<List<ChecklistPart>> getAllCheckListParts(int mCosplayId){
         this.mCosplayId=mCosplayId;
-        mAllCheckListParts=mRepository.getAllCheckListParts(mCosplayId);
+        mAllCheckListParts= mCheckListPartRepository.getAllCheckListParts(mCosplayId);
         return mAllCheckListParts;
     }
-    public void insert(ChecklistPart checklistPart){mRepository.insert(checklistPart);}
-    public void delete(ChecklistPart checklistPart){mRepository.delete(checklistPart);}
-    public void update(ChecklistPart checklistPart){mRepository.update(checklistPart);}
+    public void insert(ChecklistPart checklistPart){
+        mCheckListPartRepository.insert(checklistPart);}
+    public void delete(ChecklistPart checklistPart){
+        mCheckListPartRepository.delete(checklistPart);}
+    public void update(ChecklistPart checklistPart){
+        mCheckListPartRepository.update(checklistPart);}
 }
