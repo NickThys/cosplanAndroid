3d90cd35b06cae840c590323916154e8f8514aa0
nick <s114623@ap.be>
2020-12-08 18:23:08 +0100

    Refactored the repository

--- app/src/main/java/com/example/cosplan/data/cosplay/events/EventRepository.java
+++ app/src/main/java/com/example/cosplan/data/cosplay/events/EventRepository.java
@@ -10,54 +10,74 @@ import com.example.cosplan.data.cosplay.CosplayDatabase;
 import java.util.List;
 
 public class EventRepository {
-    private EventDao mEventDao;
+    private final EventDao mEventDao;
     private LiveData<List<Event>> mAllEvents;
     private int mCosplayId;
     private String mEventType;
-    public EventRepository(Application application){
-        CosplayDatabase mDatabase=CosplayDatabase.getDatabase(application);
-        mEventDao=mDatabase.mEventDao();
-        mAllEvents=mEventDao.getEventsByType(mCosplayId,mEventType);
+
+    public EventRepository(Application application) {
+        CosplayDatabase mCosplayDatabase = CosplayDatabase.getDatabase(application);
+        mEventDao = mCosplayDatabase.mEventDao();
+        mAllEvents = mEventDao.getEventsByType(mCosplayId, mEventType);
     }
-    LiveData<List<Event>> getAllEvents(int mCosplayId,String mEventType){
-        this.mCosplayId=mCosplayId;
-        this.mEventType=mEventType;
-        mAllEvents=mEventDao.getEventsByType(mCosplayId,mEventType);
+
+    LiveData<List<Event>> getAllEvents(int mCosplayId, String mEventType) {
+        this.mCosplayId = mCosplayId;
+        this.mEventType = mEventType;
+        mAllEvents = mEventDao.getEventsByType(mCosplayId, mEventType);
         return mAllEvents;
     }
 
-    public void insert(Event mEvent){new insertAsyncTask(mEventDao).execute(mEvent);}
-    private class insertAsyncTask extends AsyncTask<Event,Void,Void> {
-        private EventDao mDao;
-        public insertAsyncTask(EventDao mEventDao) {mDao=mEventDao;}
+    public void insert(Event mEvent) {
+        new insertAsyncTask(mEventDao).execute(mEvent);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Event, Void, Void> {
+        private final EventDao mEventDao;
+
+        public insertAsyncTask(EventDao mEventDao) {
+            this.mEventDao = mEventDao;
+        }
 
         @Override
         protected Void doInBackground(Event... events) {
-            mDao.insert(events[0]);
+            mEventDao.insert(events[0]);
             return null;
         }
     }
 
-    public void update(Event mEvent){new updateAsyncTask(mEventDao).execute(mEvent);}
-    private class updateAsyncTask extends AsyncTask<Event,Void,Void> {
-        private EventDao mDao;
-        public updateAsyncTask(EventDao mEventDao) {mDao=mEventDao;}
+    public void update(Event mEvent) {
+        new updateAsyncTask(mEventDao).execute(mEvent);
+    }
+
+    private static class updateAsyncTask extends AsyncTask<Event, Void, Void> {
+        private final EventDao mEventDao;
+
+        public updateAsyncTask(EventDao mEventDao) {
+            this.mEventDao = mEventDao;
+        }
 
         @Override
         protected Void doInBackground(Event... events) {
-            mDao.update(events[0]);
+            mEventDao.update(events[0]);
             return null;
         }
     }
 
-    public void delete(Event mEvent){new deleteAsyncTask(mEventDao).execute(mEvent);}
-    private class deleteAsyncTask extends AsyncTask<Event,Void,Void> {
-        private EventDao mDao;
-        public deleteAsyncTask(EventDao mEventDao) {mDao=mEventDao;}
+    public void delete(Event mEvent) {
+        new deleteAsyncTask(mEventDao).execute(mEvent);
+    }
+
+    private static class deleteAsyncTask extends AsyncTask<Event, Void, Void> {
+        private final EventDao mEventDao;
+
+        public deleteAsyncTask(EventDao mEventDao) {
+            this.mEventDao = mEventDao;
+        }
 
         @Override
         protected Void doInBackground(Event... events) {
-            mDao.delete(events[0]);
+            mEventDao.delete(events[0]);
             return null;
         }
     }
