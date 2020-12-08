f02ac2161b8ee96803d9638b98eb6805d59db0db
nick <s114623@ap.be>
2020-12-08 22:56:15 +0100

    Refactored the view model

--- app/src/main/java/com/example/cosplan/data/cosplay/webshop/WebshopViewModel.java
+++ app/src/main/java/com/example/cosplan/data/cosplay/webshop/WebshopViewModel.java
@@ -9,20 +9,31 @@ import androidx.lifecycle.LiveData;
 import java.util.List;
 
 public class WebshopViewModel extends AndroidViewModel {
-    private WebshopRepository mRepository;
+    private final WebshopRepository mWebshopRepository;
     LiveData<List<Webshop>> mAllWebshops;
     private int mCosplayId;
+
     public WebshopViewModel(@NonNull Application application) {
         super(application);
-        mRepository=new WebshopRepository(application);
-        mAllWebshops=mRepository.getAllWebshops(mCosplayId);
+        mWebshopRepository = new WebshopRepository(application);
+        mAllWebshops = mWebshopRepository.getAllWebshops(mCosplayId);
     }
-    public LiveData<List<Webshop>>getAllWebshops(int mCosplayId){
-        this.mCosplayId=mCosplayId;
-        mAllWebshops=mRepository.getAllWebshops(mCosplayId);
+
+    public LiveData<List<Webshop>> getAllWebshops(int mCosplayId) {
+        this.mCosplayId = mCosplayId;
+        mAllWebshops = mWebshopRepository.getAllWebshops(mCosplayId);
         return mAllWebshops;
     }
-    public void insert(Webshop webshop){mRepository.insert(webshop);}
-    public void delete(Webshop webshop){mRepository.delete(webshop);}
-    public void update(Webshop webshop){mRepository.update(webshop);}
+
+    public void insert(Webshop webshop) {
+        mWebshopRepository.insert(webshop);
+    }
+
+    public void delete(Webshop webshop) {
+        mWebshopRepository.delete(webshop);
+    }
+
+    public void update(Webshop webshop) {
+        mWebshopRepository.update(webshop);
+    }
 }
