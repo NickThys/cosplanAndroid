0f8f72a3e4fc8a1c7fbd7b44fcfa5447cf0989a2
nick <s114623@ap.be>
2020-12-08 22:24:41 +0100

    Refactored the Repository

--- app/src/main/java/com/example/cosplan/data/cosplay/refImg/ReferenceImgRepository.java
+++ app/src/main/java/com/example/cosplan/data/cosplay/refImg/ReferenceImgRepository.java
@@ -10,42 +10,54 @@ import com.example.cosplan.data.cosplay.CosplayDatabase;
 import java.util.List;
 
 public class ReferenceImgRepository {
-    private ReferenceImgDao mReferenceImgDao;
+    private final ReferenceImgDao mReferenceImgDao;
     private LiveData<List<ReferenceImg>> mAllRefImg;
     private int mCosplayId;
 
-    ReferenceImgRepository(Application application){
-        CosplayDatabase db=CosplayDatabase.getDatabase(application);
-        mReferenceImgDao=db.mReferenceImgDao();
-        mAllRefImg=mReferenceImgDao.getReferenceImg(mCosplayId);
+    ReferenceImgRepository(Application application) {
+        CosplayDatabase mCosplayDatabase = CosplayDatabase.getDatabase(application);
+        mReferenceImgDao = mCosplayDatabase.mReferenceImgDao();
+        mAllRefImg = mReferenceImgDao.getReferenceImg(mCosplayId);
     }
-    LiveData<List<ReferenceImg>> getAllRefImg(int mCosplayId){
-        this.mCosplayId=mCosplayId;
-        mAllRefImg=mReferenceImgDao.getReferenceImg(mCosplayId);
+
+    LiveData<List<ReferenceImg>> getAllRefImg(int mCosplayId) {
+        this.mCosplayId = mCosplayId;
+        mAllRefImg = mReferenceImgDao.getReferenceImg(mCosplayId);
         return mAllRefImg;
     }
-    public void insert(ReferenceImg referenceImg){new insertAsyncTask(mReferenceImgDao).execute(referenceImg);}
 
-    public void delete(ReferenceImg referenceImg) {new deleteAsyncTask(mReferenceImgDao).execute(referenceImg);}
+    public void insert(ReferenceImg referenceImg) {
+        new insertAsyncTask(mReferenceImgDao).execute(referenceImg);
+    }
 
-    private class insertAsyncTask extends AsyncTask<ReferenceImg,Void,Void> {
-        private ReferenceImgDao dao;
-        public insertAsyncTask(ReferenceImgDao mReferenceImgDao) {dao=mReferenceImgDao;}
+    public void delete(ReferenceImg referenceImg) {
+        new deleteAsyncTask(mReferenceImgDao).execute(referenceImg);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<ReferenceImg, Void, Void> {
+        private final ReferenceImgDao mReferenceImgDao;
+
+        public insertAsyncTask(ReferenceImgDao mReferenceImgDao) {
+            this.mReferenceImgDao = mReferenceImgDao;
+        }
 
         @Override
         protected Void doInBackground(ReferenceImg... referenceImgs) {
-            dao.insert(referenceImgs[0]);
+            mReferenceImgDao.insert(referenceImgs[0]);
             return null;
         }
     }
 
-    private class deleteAsyncTask extends AsyncTask<ReferenceImg,Void,Void>{
-        ReferenceImgDao dao;
-        public deleteAsyncTask(ReferenceImgDao mReferenceImgDao) {dao=mReferenceImgDao;}
+    private static class deleteAsyncTask extends AsyncTask<ReferenceImg, Void, Void> {
+        final ReferenceImgDao mReferenceImgDao;
+
+        public deleteAsyncTask(ReferenceImgDao mReferenceImgDao) {
+            this.mReferenceImgDao = mReferenceImgDao;
+        }
 
         @Override
         protected Void doInBackground(ReferenceImg... referenceImgs) {
-            dao.delete(referenceImgs[0]);
+            mReferenceImgDao.delete(referenceImgs[0]);
             return null;
         }
     }
