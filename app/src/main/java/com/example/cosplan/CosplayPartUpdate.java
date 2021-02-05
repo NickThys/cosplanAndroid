fb12813a44e97db844841d08fedbcf90719e9753
nick <s114623@ap.be>
2021-02-05 12:31:43 +0100

    Created a activity for the part update

---
+++ app/src/main/java/com/example/cosplan/CosplayPartUpdate.java
@@ -0,0 +1,14 @@
+package com.example.cosplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CosplayPartUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cosplay_part_update);
+    }
+}
