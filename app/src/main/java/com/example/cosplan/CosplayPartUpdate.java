2303dae345232e21e71def9da7a22c99b1212fbd
nick <s114623@ap.be>
2021-02-05 12:34:34 +0100

    added the code for receiving the data

--- app/src/main/java/com/example/cosplan/CosplayPartUpdate.java
+++ app/src/main/java/com/example/cosplan/CosplayPartUpdate.java
@@ -2,7 +2,10 @@ package com.example.cosplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.cosplan.data.cosplay.part.Part;
 
 public class CosplayPartUpdate extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class CosplayPartUpdate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cosplay_part_update);
+        Intent intent=getIntent();
+        Part part=intent.getParcelableExtra("part");
     }
 }
